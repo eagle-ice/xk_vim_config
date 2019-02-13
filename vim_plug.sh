23e5cfba67aac30fc87e7214093146584d80c769
xk.yang <xk.yang@mstarsemi.com>
2019-02-13 22:25:31 +0800

    Add vim 安装和编译以及依赖关系说明
    Add vim 安装和编译以及依赖关系说明

--- vim_plug.sh
+++ vim_plug.sh
@@ -1,5 +1,22 @@
 #/bin/bash
 
+if [ ! -d ${HOME}/.vim/source ]; then
+    mkdir -p ${HOME}/.vim/source;
+    if [ ! -d ${HOME}/.vim/source/vim ]; then
+        #sudo apt install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev
+        #libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev python3-dev
+        #ruby-dev lua5.1 liblua5.1-dev libperl-dev git
+        cd ${HOME}/.vim/source
+        git clone https://github.com/vim/vim.git vim
+        cd ${HOME}/.vim/source/vim
+        ./configure --with-features=huge --prefix=${HOME}/vim/
+        make;make install
+        echo "export PATH=\$HOME/vim/bin:\$PATH" >> $HOME/.bashrc
+    fi
+fi
+
+cd ${HOME}/.vim/
+
 VIM_CONFIG_PATH=${HOME}/.vim/vimrc
 
 if [ ! -f "${HOME}/.vim/autoload/plug.vim" ]; then
@@ -30,8 +47,8 @@ source ./xk_vim_config/base_config.sh ${VIM_CONFIG_PATH}
 
 for config_file in `find ./xk_vim_config/ -name "*.sh"`
 do
-	if [ x"$config_file" != x"./xk_vim_config/base_config.sh" ]; then
-		echo $config_file
-		source $config_file ${VIM_CONFIG_PATH}
-	fi
+    if [ x"$config_file" != x"./xk_vim_config/base_config.sh" ]; then
+        echo $config_file
+        source $config_file ${VIM_CONFIG_PATH}
+    fi
 done
